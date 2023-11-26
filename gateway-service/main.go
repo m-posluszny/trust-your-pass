9b5bf43255b5bb67fda9db81b775952ecbb9a9b3
Jakub <Lasiuk16@gmail.com>
2023-11-26 12:21:17 +0100

    Added viper module

--- gateway-service/main.go
+++ gateway-service/main.go
@@ -2,28 +2,63 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"log"
 	"net/http"
 )
 
-type password struct {
+type Password struct {
 	ID       int    `json:"id"`
 	Password string `json:"password"`
 	Strength int    `json:"strength"`
 }
 
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func loadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("application")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
+
 // temporary collection that replaces db
-var passwords = []password{
+var passwords = []Password{
 	{ID: 1, Password: "intel1", Strength: 0},
 	{ID: 2, Password: "elyass15@ajilent-ci", Strength: 2},
 	{ID: 3, Password: "hodygid757#$!23w", Strength: 1},
 }
 
 func main() {
+
+	config, err := loadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	/*conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}*/
+
 	router := gin.Default()
 	router.GET("/api/v1/passwords", getPasswords)
 	router.POST("/api/v1/passwords", postPassword)
 
-	router.Run("localhost:10000")
+	router.Run(config.ServerAddress)
 }
 
 func getPasswords(c *gin.Context) {
@@ -31,7 +66,7 @@ func getPasswords(c *gin.Context) {
 }
 
 func postPassword(c *gin.Context) {
-	var newPassword password
+	var newPassword Password
 	if err := c.BindJSON(&newPassword); err != nil {
 		return
 	}
