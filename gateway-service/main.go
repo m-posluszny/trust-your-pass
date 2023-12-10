45ef506ce1e494313d424219913955e8a7d21de1
Jakub <Lasiuk16@gmail.com>
2023-12-10 01:32:39 +0100

    Updated get and post endpoints

--- gateway-service/main.go
+++ gateway-service/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"inf/gateway-service/configs"
@@ -16,11 +17,34 @@ import (
 //TODO use interfaces, controllers, interfacesImpl
 //TODO put events to rabbitmq, add listener
 //TODO tests
+//TODO golang app docker compose
+
+//TODO wątek dla rabbitmq
 
 type PasswordDto struct {
-	//Id       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Password string `json:"password" bson:"password"`
-	Strength int    `json:"strength" bson:"strength"`
+	Id          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Password    string             `json:"password,omitempty" bson:"password,omitempty"`
+	Strength    int                `json:"strength" bson:"strength"`
+	IsProcessed bool               `json:"isProcessed" bson:"isProcessed"`
+}
+
+type PreconditionDto struct {
+	ConditionName string `json:"condition"`
+	IsSatisfied   bool   `json:"isSatisfied"`
+}
+
+func NewPreconditionDto(condition string) *PreconditionDto {
+	return &PreconditionDto{condition, false}
+}
+
+type PostResponseDto struct {
+	Id            interface{}     `json:"id" bson:"id"`
+	Preconditions map[string]bool `json:"preconditions" bson:"preconditions"`
+	IsProcessed   bool            `json:"isProcessed" bson:"isProcessed"`
+}
+
+func NewPostResponseDto(preconditions map[string]bool) *PostResponseDto {
+	return &PostResponseDto{nil, preconditions, false}
 }
 
 var PasswordCollection *mongo.Collection
@@ -35,17 +59,21 @@ func main() {
 	defer MongoClient.Disconnect(context.TODO())
 
 	RmqConnection, RmqChannel = configs.InitRabbitMQConnection()
+	defer RmqConnection.Close()
 
 	router := gin.Default()
-	router.GET("/api/v1/passwords", getPasswords)
-	router.GET("/api/v1/passwords/:password", getPasswordStrength)
-	router.GET("/api/v1/passwords/:password/poll", pollResult)
-	router.POST("/api/v1/passwords", postPassword)
+	router.GET("/api/v1/passwords", getAll)
+	router.GET("/api/v1/passwords/:password", getByPassword)
+	router.POST("/api/v1/passwords", insert)
+
+	//TODO should be done by listener instead?
+	router.PATCH("/api/v1/passwords/:password/update", updateStrength)
 
 	router.Run(configs.EnvList.ServerAddress)
 }
 
-func getPasswords(c *gin.Context) {
+func getAll(c *gin.Context) {
+	//get all records that were processed
 	cursor, err := PasswordCollection.Find(c, bson.M{"strength": bson.M{"$ne": -1}})
 	if err != nil {
 		c.AbortWithError(http.StatusNotFound, err)
@@ -57,10 +85,12 @@ func getPasswords(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, results)
 }
 
-func getPasswordStrength(c *gin.Context) {
+func getByPassword(c *gin.Context) {
 	password := c.Param("password")
 
-	isValid, preconditions := validateInput(password)
+	preconditions := validateInput(password)
+	isValid := arePreconditionsSatisfied(preconditions)
+	//why do I use 400 in that case: https://stackoverflow.com/questions/3290182/which-status-code-should-i-use-for-failed-validations-or-invalid-duplicates
 	if !isValid {
 		c.JSON(http.StatusBadRequest, preconditions)
 		return
@@ -72,62 +102,87 @@ func getPasswordStrength(c *gin.Context) {
 		c.IndentedJSON(http.StatusNotFound, err.Error())
 		return
 	}
-	c.IndentedJSON(http.StatusOK, queryResult.Strength)
+	c.IndentedJSON(http.StatusOK, PasswordDto{
+		Id:          queryResult.Id,
+		Strength:    queryResult.Strength,
+		IsProcessed: queryResult.IsProcessed,
+	})
 }
 
-func postPassword(c *gin.Context) {
+func insert(c *gin.Context) {
 	requestBody := PasswordDto{}
-
-	//bind req body
 	if err := c.BindJSON(&requestBody); err != nil {
 		c.AbortWithError(http.StatusBadRequest, err)
 		return
 	}
 
 	//validate if string consists of all spaces, etc
-	isValid, preconditions := validateInput(requestBody.Password)
+	preconditions := validateInput(requestBody.Password)
+	isValid := arePreconditionsSatisfied(preconditions)
+	//why do I use 400 in that case: https://stackoverflow.com/questions/3290182/which-status-code-should-i-use-for-failed-validations-or-invalid-duplicates
 	if !isValid {
-		c.JSON(http.StatusBadRequest, preconditions)
+		c.JSON(http.StatusBadRequest, PostResponseDto{
+			Id:            nil,
+			Preconditions: preconditions,
+			IsProcessed:   false,
+		})
 		return
 	}
-
 	filter := bson.D{{"password", requestBody.Password}}
-	update := bson.D{{"$set", bson.D{{"password", requestBody.Password}, {"strength", requestBody.Strength}}}}
+	update := bson.D{{"$set", bson.D{{"password", requestBody.Password}, {"strength", -1}, {"isProcessed", false}}}}
 	opts := options.Update().SetUpsert(true)
 	result, err := PasswordCollection.UpdateOne(context.TODO(), filter, update, opts)
-
 	if err != nil {
-		c.AbortWithError(http.StatusBadRequest, err)
+		//mongo query issue - unlikely to happen
+		c.JSON(http.StatusInternalServerError, err)
 		return
 	}
-	c.JSON(http.StatusCreated, result.UpsertedID)
+
+	//TODO id+preconditions
+	c.JSON(http.StatusCreated, PostResponseDto{
+		Id:            result.UpsertedID,
+		Preconditions: preconditions,
+		IsProcessed:   false,
+	})
 }
 
-func validateInput(password string) (bool, string) {
-	var preconditions []string
+const lengthCondition = "Password should contain at least 4 signs"
+const nonEmptyStringCondition = "Provided string is not empty"
+const nonWhitespacesOnlyCondition = "Provided string doesnt consists of whitespaces only"
+const notAllowedSymbolsCondition = "Provided string doesnt contain symbols that are not allowed"
 
-	if len(password) <= 3 {
-		preconditions = append(preconditions, "Password should contain at least 4 signs")
+func validateInput(password string) map[string]bool {
+
+	var preconditions = map[string]bool{
+		lengthCondition:             false,
+		nonEmptyStringCondition:     false,
+		nonWhitespacesOnlyCondition: false,
+		notAllowedSymbolsCondition:  false}
+
+	if len(password) >= 4 {
+		preconditions[lengthCondition] = true
 	}
-	if stringUtils.IsBlank(password) {
-		preconditions = append(preconditions, "Provided string is empty")
+	if !stringUtils.IsBlank(password) {
+		preconditions[nonEmptyStringCondition] = true
 	}
-	if stringUtils.IsWhitespace(password) {
-		preconditions = append(preconditions, "Provided string consists of whitespaces only")
+	if !stringUtils.IsWhitespace(password) {
+		preconditions[nonWhitespacesOnlyCondition] = true
 	}
-	if stringUtils.ContainsAny(password, "\\0") {
-		preconditions = append(preconditions, "Provided string contains not allowed symbols")
+	if !stringUtils.ContainsAny(password, "\\0") {
+		preconditions[notAllowedSymbolsCondition] = true
 	}
 
-	if len(preconditions) == 0 {
-		return true, ""
-	} else {
-		return false, stringUtils.Join(preconditions, "\n")
-	}
+	return preconditions
 }
 
-// might be unused if model will return results at fast manner
-func pollResult(c *gin.Context) {
-	password := c.Param("password")
-	password += ""
+func arePreconditionsSatisfied(preconditions map[string]bool) bool {
+	for _, v := range preconditions {
+		if v == false {
+			return false
+		}
+	}
+	return true
+}
+
+func updateStrength(c *gin.Context) {
 }
