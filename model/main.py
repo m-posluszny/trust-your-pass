2b685162b06b87d0bea9ce22ab1ff242558a695b
Lasiuk <56473217+JakKopec@users.noreply.github.com>
2024-01-10 19:40:48 +0100

    Removed old model service (#5)

--- model/main.py
+++ model/main.py
@@ -1,57 +1,68 @@
 import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
+from dotenv import load_dotenv
 import pickle
 from typing import Union
+import json
 import pika, sys, os
 
-strenght_mapper = [3,2,4,0,1]
-model_file__name = 'password_model_v2.keras'
-tokenizer_file_name = 'tokenizer_v2.pickle'
-encoder_file_name = 'label_encoder_v2.pickle'
+load_dotenv(dotenv_path="../tools/services.env")
+strength_mapper = [3, 2, 4, 0, 1]
+
+model_file__name = 'model.keras'
+tokenizer_file_name = 'tokenizer.pickle'
+encoder_file_name = 'label_encoder.pickle'
 model = load_model(model_file__name)
-# loading
+
+# loading tokenizer
 with open(tokenizer_file_name, 'rb') as handle:
     tokenizer = pickle.load(handle)
-    
+
+# loading label_encoder    
 with open(encoder_file_name, 'rb') as handle:
     label_encoder = pickle.load(handle)
-    
-def predict_password_strength(new_password):
-    new_sequence = tokenizer.texts_to_sequences([new_password])
-    new_padded_sequence = pad_sequences(new_sequence,220)
+
+
+def predict_password_strength(jsonString):
+    jsonDict = json.loads(jsonString)
+    new_sequence = tokenizer.texts_to_sequences([jsonDict.get("password")])
+    new_padded_sequence = pad_sequences(new_sequence, 232)
     prediction = model.predict(new_padded_sequence, verbose=None)
     predicted_class_index = np.argmax(prediction)
     predicted_strength = label_encoder.classes_[predicted_class_index]
-    result = f'Password: "{new_password}", strenght: {predicted_strength}, mapped_class:  [{strenght_mapper[predicted_class_index]}]' 
-    print(result)
-    return result
-    
-def send_result(result):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue='result')
-    channel.basic_publish(exchange='', routing_key='result', body=result)
+    print(
+        f' [process]  Password: "{jsonDict.get("password")}", strength: {predicted_strength}, mapped_strength:  {strength_mapper[predicted_class_index]}')
+
+    print(f' [process]  Password: "{jsonDict.get("password")}"')
+    return json.dumps({"_id": jsonDict.get("_id"), "strength": strength_mapper[predicted_class_index]})
+
+def send_result(channel, modelOutQueueName, result):
     print(f" [x] Sent result: '{result}'")
-    connection.close()
+    channel.basic_publish(exchange='', routing_key=modelOutQueueName, body=result)
+
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    modelInQueueName = os.getenv("RABBITMQ_MODEL_IN_QUEUE_NAME")
+    modelOutQueueName = os.getenv("RABBITMQ_MODEL_OUT_QUEUE_NAME")
+    connection = pika.BlockingConnection(pika.URLParameters(os.getenv("RABBITMQ_CONNECTION_STRING")))
     channel = connection.channel()
-
-    channel.queue_declare(queue='password_to_process')
+    channel.queue_declare(queue=modelInQueueName, durable=True)
+    channel.queue_declare(queue=modelOutQueueName, durable=True)
 
     def callback(ch, method, properties, body):
         decoded_string = body.decode("utf-8")
         print(f" [x] Received {decoded_string}")
         result = predict_password_strength(decoded_string)
-        send_result(result)
+        send_result(channel, modelOutQueueName, result)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue=modelInQueueName, on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
-    
+    channel.connection.close()
+
+
 if __name__ == '__main__':
     try:
         main()
